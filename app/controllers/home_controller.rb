2da6ce02659e768a7ad6866275d25ac3c3ff2f75
Nik Krimm <nkrimm@nulllenny.dreamhost.com>
2010-07-12 21:28:18 -0700

    Revert "correcting bad globbing juju"
    This reverts commit b9665bbb3df3987062dc93ed4c312c3d31d847e4.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,13 @@ class HomeController < ApplicationController
   def index
     # read the list of files in the band photos dir
     # and print them to simple array
-    path = RAILS_ROOT + '/public/images/photos/band/**'
-    d = Dir.glob(path) 
+    path = RAILS_ROOT + '/public/images/photos/band'
+    d = Dir.new path
     @photos = d.map do |n|
       '/images/photos/band/' + n
     end
+    @photos.shift  # cheap-ass way to remove '.'
+    @photos.shift  # cheap-ass way to remove '..'
     @photos = @photos.sort_by {rand}
   end
 
