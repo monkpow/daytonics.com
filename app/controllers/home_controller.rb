7f4cec2fb0e77da439b0447605eb67898d5632a9
nkrimm <nkrimm@gmail.com>
2010-07-12 23:19:46 -0500

    adding new photos, randomizing photo array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,13 @@ class HomeController < ApplicationController
     # read the list of files in the band photos dir
     # and print them to simple array
     path = RAILS_ROOT + '/public/images/photos/band'
-    d = Dir.new path 
-    @photos = d.map do |n| 
-      '/images/photos/band/' + n 
+    d = Dir.new path
+    @photos = d.map do |n|
+      '/images/photos/band/' + n
     end
-    @photos.shift
-    @photos.shift
-
+    @photos.shift  # cheap-ass way to remove '.'
+    @photos.shift  # cheap-ass way to remove '..'
+    @photos = @photos.sort_by {rand}
   end
 
 end
